d903e5d5ad0962ad3f22b0dcd347b882c81f121d
leo <lionel126@qq.com>
2021-08-10 18:21:24 +0800

    remove " in .env

--- conftest.py
+++ conftest.py
@@ -1,10 +1,10 @@
 
 def pytest_terminal_summary(terminalreporter, exitstatus, config):
-    """统计次数结果"""
+    """统计测试结果发钉钉"""
     s = ''
     for k, v in terminalreporter.stats.items():
         if k != '':
             s += f'{k}: {len(v)}, '
     s = s[:-2]
     with open('.env', 'w') as f:
-        f.write(f'RESULTS="{s}"\n')
+        f.write(f'RESULTS={s}\n')
