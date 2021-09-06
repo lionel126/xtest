2e34e3d9e3d545dca5176c169486f9248002423c
leo <lionel126@qq.com>
2021-09-06 17:38:25 +0800

    add test env file

---
+++ config_local.example.py
@@ -0,0 +1,5 @@
+TESTAPI_BASE = 'https://t.vmovier.cc'
+BASE_URL = 'http://10.25.98.5:5004/mall/v2'
+SEARCH_BASE_URL = 'http://10.25.98.5:5006/rpc/pub/v1/search'
+PAY_ADMIN_BASE_URL = 'https://pay-admin.vmovier.cc'
+X_USER_TOKEN = 'eyAiaWQiOiAiMSIsICJ1c2VybmFtZSI6ICJ6aGFuZ3NhbiIsICJuaWNrbmFtZSI6ICJ6aGFuZ3NhbiIsICJlbWFpbCI6ICJ6aGFuZ3NhbkB4aW5waWFuY2hhbmcuY29tIiB9.ed5833edd7bb91dbb89bb3b0f99788db200fd052'
